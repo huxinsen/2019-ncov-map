5c3553abbcc2889327ed6d332ed3932dde6df9bc
huxinsen <huxinsen@bytedance.com>
2020-02-14 20:07:44 +0800

    build: release 0.1

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <MyMap />
+  </div>
+</template>
+
+<script>
+import MyMap from './components/MyMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyMap,
+  },
+}
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
